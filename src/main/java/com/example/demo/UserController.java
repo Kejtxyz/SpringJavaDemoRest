141ef0b5362288ebe1f3fb02b238c4878e079277
kasia <kgbsz@wp.pl>
2019-04-17 19:16:50 +0200

    Spring Pierwsze kroki microserwis - pierwsze zadanie 1.utworzenie testu 2.utworzenie controllera user
    3COntroller nic nie zwraca

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,4 +1,14 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/users")
+    public void getAll(){
+
+    }
 }
